db9a6a02d34292315504a9d990aa253ea8ed040d
imleog <118559187+imleog@users.noreply.github.com>
2022-11-18 11:37:08 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,60 @@
+const $video = document.querySelector('#video')
+const $play = document.querySelector('#play')
+const $pause = document.querySelector('#pause')
+const $backward = document.querySelector('#backward')
+const $forward = document.querySelector('#forward')
+
+
+$play.addEventListener('click',handlePlay)
+$pause.addEventListener('click',handlePause)
+
+function handlePlay(){
+    $video.play()
+    $play.hidden = true
+    $pause.hidden = false
+    console.log('le diste click al botón de play')
+}
+
+function handlePause(){
+    $video.pause()
+    $pause.hidden = true
+    $play.hidden = false
+    console.log('le diste click al botón de pause')
+}
+
+$backward.addEventListener('click',handelBackward)
+
+function handelBackward(){
+    //$video.currentTime = $video.currentTime - 10
+    $video.currentTime -= 10
+    console.log('para atrás 10 segundos',$video.currentTime)
+}
+
+$forward.addEventListener('click',handleForeward)
+
+function handleForeward(){
+    //$video.currentTime = $video.currentTime + 10
+    $video.currentTime += 10
+    console.log('para adelante 10 segundos',$video.currentTime)
+}
+
+const $progress = document.querySelector('#progress')
+$video.addEventListener('loadedmetadata', handleLoaded)
+$video.addEventListener('timeupdate', handleTimeUpdate)
+
+function handleLoaded(){
+    $progress.max = $video.duration
+    console.log('Ha cargado mi video', $video.duration)
+}
+
+function handleTimeUpdate(){
+    $progress.value = $video.currentTime
+    // console.log('holi', $video.currentTime)
+}
+
+$progress.addEventListener('input', handleInput)
+
+function handleInput(){
+    $video.currentTime = $progress.value
+    console.log($progress.value)
+}
